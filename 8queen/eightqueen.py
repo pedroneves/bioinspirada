c3ac7caac77100d8e982ce93165b49378ec70a18
Pedro Neves <pedro.hfn7@gmail.com>
2016-04-25 22:52:21 -0300

    functions update

--- 8queen/eightqueen.py
+++ 8queen/eightqueen.py
@@ -1,13 +1,16 @@
 import random
+from copy import copy
 from genetics import Genetics
 import sys
 import os
+import json
 
 current_module = sys.modules[__name__]
 
 testAmount = 10
 avglog = []
 maxlog = []
+savingJson = True
 isPlotting = True
 run = True
 
@@ -17,11 +20,14 @@ mutprob = 0.8
 popcap = 100
 initializationfnid = 'randominit'
 fitnessfnid = 'collisionFitness'
-selectionfnid = 'rank2outof5random'
-recombinationfnid = 'cutAndCrossfill'
+selectionfnid = 'tournament'
+recombinationfnid = 'pmx'
 mutationfnid = 'swapMutation'
 survivingfnid = 'replaceWorst'
 stopfnid = 'stop'
+# tournament parameters
+nSelected = 5
+nParents = 2
 
 ###############################
 # AUX
@@ -103,6 +109,25 @@ def logger (iters, pop):
     avglog.append(avgfitness(pop))
     maxlog.append(maxfitness(pop))
 
+def jsonMaxAvgLog (avglog, maxlog):
+    data = {}
+    data['iterations'] = len(avglog)
+    data['avglog'] = avglog
+    data['maxlog'] = maxlog
+
+    # Creates a dir to contain the ploting graphs for the current configuration
+    plotDir = 'plot-' + str(initializationfnid) + '-' + str(fitnessfnid) + '-' + str(selectionfnid) + '-' + str(recombinationfnid) + '-' + str(mutationfnid) + '-' + str(survivingfnid) + '-' + 'mit' + str(maxiterations) + '-' + 'mtp' + str(mutprob) + '-' + 'cap' + str(popcap)
+    if not os.path.exists(plotDir):
+        os.makedirs(plotDir)
+
+    # Checks the plotting order
+    order = 1
+    plotDirFiles = [int(f.split(".")[0]) for f in os.listdir(plotDir) if (os.path.isfile(os.path.join(plotDir, f)) and f.split(".")[1] == "json")]
+    order = 1 + (max(plotDirFiles) if len(plotDirFiles) > 0 else 0)
+
+    with open(os.path.join(plotDir, str(order) + '.json'), 'w') as outfile:
+        json.dump(data, outfile)
+
 def plotMaxAvgLog (avglog, maxlog):
 
     # Creates a dir to contain the ploting graphs for the current configuration
@@ -112,8 +137,8 @@ def plotMaxAvgLog (avglog, maxlog):
 
     # Checks the plotting order
     order = 1
-    plotDirFiles = [f for f in os.listdir(plotDir) if os.path.isfile(os.path.join(plotDir, f))]
-    order = len(plotDirFiles) + 1
+    plotDirFiles = [int(f.split(".")[0]) for f in os.listdir(plotDir) if (os.path.isfile(os.path.join(plotDir, f)) and f.split(".")[1] == "png")]
+    order = 1 + (max(plotDirFiles) if len(plotDirFiles) > 0 else 0)
 
     try:
         import matplotlib
@@ -134,7 +159,49 @@ def plotMaxAvgLog (avglog, maxlog):
         print 'attection: impossible to plot the grafic, verify if you have matplotlib installed.'
         print 'What went wrong: '+ str(e)
 
+# Find the elements selected by a roulette. SortedPop is the decreasingly sorted population,
+# pointers is a list of roulette results, between 0 and the fitness sum of the population.
+def findRouletterPointers (sortedPop, pointers):
+    ret = list()
 
+    for pointer in pointers:
+        cumFitness = 0
+        position = 0
+        while cumFitness + sortedPop[position]['fitness'] < pointer:
+            cumFitness += sortedPop[position]['fitness']
+            position += 1
+        ret.append(sortedPop[position])
+
+    return ret
+
+def pmxGenerateChild (p1, p2, idx1, idx2):
+    child = dict()
+
+    def findPosition(phenotype, elem):
+        pos = 0
+        for e in phenotype:
+            if e == elem:
+                return pos
+            pos += 1
+        return -1
+
+    for i in range(idx1, idx2+1):
+        child[i] = p1[i]
+
+    for i_ in range(idx1, idx2+1):
+        i = i_
+        elem = p2[i_]
+        if not (elem in child.values()):
+            while i in child.keys():
+                j = child[i]
+                i = findPosition(p2, j)
+            child[i] = elem
+
+    # Fill the remaining slots of the child and return it as a list
+    ret = copy(p2)
+    for i in child:
+        ret[i] = child[i]
+    return ret
 
 
 ###############################
@@ -176,6 +243,31 @@ def randominit ():
 ###############################
 # RECOMBINATIONS
 ###############################
+
+def pmx (parents):
+    p1 = gtp(parents[0]['genotype'])
+    p2 = gtp(parents[1]['genotype'])
+
+    # Generate two distinct indices
+    positions = set()
+    while len(positions) < 2:
+        positions.add(random.randint(0, len(p1)-1))
+    idx1 = min(positions)
+    idx2 = max(positions)
+
+    child1Phenotype = pmxGenerateChild(p1, p2, idx1, idx2)
+    child2Phenotype = pmxGenerateChild(p2, p1, idx1, idx2)
+
+    child1 = dict()
+    child1['genotype'] = ptg(child1Phenotype)
+    child1['fitness'] = fitnessfn(child1['genotype'])
+
+    child2 = dict()
+    child2['genotype'] = ptg(child2Phenotype)
+    child2['fitness'] = fitnessfn(child2['genotype'])
+
+    return [child1, child2]
+
 def cutAndCrossfill (parents):
     childGeno1 = []
     childGeno2 = []
@@ -241,27 +333,80 @@ def simpleCrossover (parents):
 
     return r
 
-
-
 ###############################
 # MUTATIONS
 ###############################
 
+def reversionMutation (solution):
+    phenotype = gtp(solution['genotype'])
+
+    # No mutation occurs
+    if random.random() > mutprob:
+        return solution
+
+    # Generate two distinct indices
+    positions = set()
+    while len(positions) < 2:
+        positions.add(random.randint(0, len(phenotype)-1))
+    idx1 = min(positions)
+    idx2 = max(positions)
+
+    newPhenotype = list()
+    newPhenotype.extend(phenotype[:idx1])
+    middle = phenotype[idx1:idx2+1]
+    middle.reverse()
+    newPhenotype.extend(middle)
+    newPhenotype.extend(phenotype[idx2+1:])
+
+    # Compute the new genotype and fitness
+    genotype = ptg(newPhenotype)
+    fitness = fitnessfn(genotype)
+    # Update values and return
+    solution['genotype'] = genotype
+    solution['fitness'] = fitness
+    return solution
+
+def insertionMutation (solution):
+    phenotype = gtp(solution['genotype'])
+
+    # No mutation occurs
+    if random.random() > mutprob:
+        return solution
+
+    # Generate two distinct indices
+    positions = set()
+    while len(positions) < 2:
+        newPosition = random.randint(0, len(phenotype)-1)
+        if not (newPosition-1 in positions):
+            positions.add(newPosition)
+    idx1 = min(positions)
+    idx2 = max(positions)
+
+    temp = phenotype[idx2]
+    phenotype.pop(idx2)
+    phenotype.insert(idx1+1, temp)
+
+    # Compute the new genotype and fitness
+    genotype = ptg(phenotype)
+    fitness = fitnessfn(genotype)
+    # Update values and return
+    solution['genotype'] = genotype
+    solution['fitness'] = fitness
+    return solution
+
 def swapMutation (solution):
     mutatedGenotype = genotypeToList(solution['genotype'])
-    i = 0
-    tgt = 0
 
-    while(i < 8):
-        if(random.random() <= mutprob):
-            while(tgt == i):
-                tgt = random.randint(0,7)
+    if random.random() <= mutprob:
+        tgt1 = random.randint(0,7)
+        tgt2 = random.randint(0,7)
 
-            tgtval = mutatedGenotype[tgt]
-            mutatedGenotype[tgt] = mutatedGenotype[i]
-            mutatedGenotype[i] = tgtval
+        while(tgt1 == tgt2):
+            tgt2 = random.randint(0,7)
 
-        i += 1
+        aux = mutatedGenotype[tgt2]
+        mutatedGenotype[tgt2] = mutatedGenotype[tgt1]
+        mutatedGenotype[tgt1] = aux
 
     r = individual()
     r['genotype'] = "".join(mutatedGenotype)
@@ -269,6 +414,7 @@ def swapMutation (solution):
 
     return r
 
+# Classic Genetic Algorithm mutation, with independent mutations occuring at each bit
 def simpleMutation (solution):
     i = 0
     mutatedGenotype = range(len(solution['genotype']))
@@ -298,6 +444,47 @@ def mutate (children):
 ###############################
 # SELECTIONS
 ###############################
+
+# Classic roulette wheel method for selecting parents
+def roulette (pop):
+    sortedPop = sorted(pop, reverse = True, key = lambda x: x['fitness'])
+    fitnessSum = sum([x['fitness'] for x in sortedPop])
+
+    roulettePointers = list();
+    roulettePointers.append(random.uniform(0, fitnessSum))
+    roulettePointers.append(random.uniform(0, fitnessSum))
+
+    return findRouletterPointers(sortedPop, roulettePointers)
+
+# Baker's Stochastic Universal Sampling roulette algorithm
+def rouletteSUS (pop):
+    sortedPop = sorted(pop, reverse = True, key = lambda x: x['fitness'])
+    fitnessSum = sum([x['fitness'] for x in sortedPop])
+
+    pointerDistance = fitnessSum/float(nParents)
+    pointer = random.uniform(0, pointerDistance)
+    roulettePointers = list()
+    while len(roulettePointers) < nParents:
+        roulettePointers.append(pointer)
+        pointer += pointerDistance
+
+    return findRouletterPointers(sortedPop, roulettePointers)
+
+# Classic tournament method for selecting parents
+def tournament (pop):
+    # Select nSelected random indices corresponding to members of the population pop
+    randIndices = list()
+    while(len(randIndices) < nSelected):
+        newRandIndex = random.randint(0, len(pop)-1)
+        if not (newRandIndex in randIndices):
+            randIndices.append(newRandIndex)
+
+    # Get the appropriate members
+    selectedMembers = map(lambda idx: pop[idx], randIndices)
+    # Sort the selected members by fitness
+    selectedMembers.sort(reverse = True, key = lambda x: x['fitness']);
+    # Return nParents elements with the highest fitness
+    return selectedMembers[:nParents]
 
 def rank2outof5random (pop):
     randoms = []
@@ -386,6 +573,9 @@ if run:
         if isPlotting:
             print "Plotting..."
             plotMaxAvgLog(avglog, maxlog)
+
+        if savingJson:
+            jsonMaxAvgLog(avglog, maxlog)
 
         best = getBestSolution(superiorRace)
         print "Test #{2}: Max fitness {0} and phenotype {1}".format(best['fitness'], gtp(best['genotype']), (test+1))
