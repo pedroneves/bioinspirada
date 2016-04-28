c7c39d543ffeab1e8f9737c684dc868b8cdd8e0e
Pedro Neves <pedro.hfn7@gmail.com>
2016-04-28 12:44:01 -0300

    module refactored to present more valuable data about the logs

--- 8queen/analyzer.py
+++ 8queen/analyzer.py
@@ -39,24 +39,79 @@ def getLogIterations (logDirectory):
 
     return iterations
 
-def avgLogIterations (l):
-    return (sum(l) + 0.0)/len(l)
+def mean (logs):
+    return (sum(logs) + 0.0)/len(logs)
 
-def sdLogIterations (l):
+def sd (logs):
     try:
         import numpy as np
-        return np.std(l)
+        return np.std(logs)
     except Exception, e:
-        print 'sdLogIterations is kaput: '+ str(e)
+        print 'sd is kaput: '+ str(e)
+
+def getLogData (logDirectory):
+    # Get the files on the given logDirectory
+    files = getLogFiles(logDirectory)
+
+    # iterate over all files, open and extract the data
+    i = 0
+    log = {
+        'strategy': logDirectory, # labeling
+        'iterations': [], # all the iterations for each test
+        'avgs': [], # all the average fitness of all tests
+        'maxs': [], # all the maximun fitness of all tests
+        'tests': 0, # total of tests for the strategy specified for by the logDirectory
+        'converged': 0, # number of converged tests
+        'statistics': {
+            'convergence': 0,
+            'iterations': {
+                'avg': 0,
+                'sd': 0
+            },
+            'fitness': {
+                'avg': 0,
+                'sd': 0
+            }
+        }
+    }
+    while i < len(files):
+        log['tests'] += 1
+
+        with open(os.path.join(logDirectory, files[i])) as json_file:
+            j = json.load(json_file)
+
+            if(j['converged']):
+                log['converged'] += 1
+                log['iterations'].append(j['iterations'])
+                log['avgs'].extend(j['avglog'])
+                log['maxs'].extend(j['maxlog'])
+        i+=1
+
+    # calculate the statistics
+    log['statistics']['convergence'] = (log['converged']*100.0)/log['tests']
+
+    log['statistics']['iterations']['avg'] = mean(log['iterations'])
+    log['statistics']['iterations']['sd'] = sd(log['iterations'])
+
+    log['statistics']['fitness']['avg'] = mean(log['avgs'])
+    log['statistics']['fitness']['sd'] = sd(log['maxs'])
+
+    return log
+
 
 
 testDirs = getLogDirs()
-testLogs = map(getLogIterations, testDirs)
-testAvgs = map(avgLogIterations, testLogs)
-testStds = map(sdLogIterations, testLogs)
+testLogs = map(getLogData, testDirs)
 
-print 'Iteration analysis'
+print 'Analysis'
 i = 0
-while i < len(testDirs):
-    print '\n{0}\n\tavg: {1}\n\tsd: {2}'.format(" ".join(testDirs[i].split("-")[1:]), testAvgs[i], testStds[i])
+while i < len(testLogs):
+    print 'Test {0}\n\tConvergence: {1}\n\tIterations\n\t  avg: {2}\n\t  sd: {3}\n\tFitness\n\t  avg: {4}\n\t  sd: {5}'.format(
+        testLogs[i]['strategy'],
+        testLogs[i]['statistics']['convergence'],
+        testLogs[i]['statistics']['iterations']['avg'],
+        testLogs[i]['statistics']['iterations']['sd'],
+        testLogs[i]['statistics']['fitness']['avg'],
+        testLogs[i]['statistics']['fitness']['sd'],
+    )
     i+=1
