1cd740eb7868c36d891edc66eb09b89c414a212e
Pedro Neves <pedro.hfn7@gmail.com>
2016-05-17 19:07:24 +0000

    setup.py will hold paramenters to be used throughout the project

---
+++ ackley/setup.py
@@ -0,0 +1,14 @@
+# Total amount of individuals
+popcap = 1
+
+# Specifies the solution's dimension
+dims = 30
+
+# Max mutation pace range for initialization
+mutpacerange = 5
+
+# Minimum mutation pace
+mutpacemin = 0.1
+
+# Evolution Strategy params
+mutationfn = "singleSd"
