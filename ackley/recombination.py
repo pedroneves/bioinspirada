51543a2582dc15ee331bee116912cd656e69a68f
rlfs <rlfs@cin.ufpe.br>
2016-05-20 13:30:54 -0300

    resolve #2 - Recombinacao discreta

--- ackley/recombination.py
+++ ackley/recombination.py
@@ -6,5 +6,7 @@ import numpy as np
 def recombIntermitante(p1, p2):
 	return (p1 + p2)/2.0
 
-
+def recombDiscret(p1, p2):
+	mask = np.random.rand(p1.shape[0]) > 0.5
+	return np.vectorize(lambda p1, p2, c: p1 if c else p2)(p1,p2,mask)
 
