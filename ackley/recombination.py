59bc3190b102aa21e230390e1ce13ed568fd7b25
joaogsma <joaogsma@gmail.com>
2016-05-20 21:54:14 -0300

    global interpolation recombination completed

--- ackley/recombination.py
+++ ackley/recombination.py
@@ -3,6 +3,8 @@ import numpy as np
 from solution import Solution
 from random import random
 
+childAsAverage = False
+
 def _discListRecomb(list1, list2):
     result = list()
     
@@ -11,13 +13,34 @@ def _discListRecomb(list1, list2):
     
     return result
 
-def _interpListRecomb(list1, list2, alpha=random()):
+def _interpListRecomb(list1, list2, alpha):
     result = list()
 
     for i in range(0, len(list1)):
         result.append(alpha*list1[i] + (1-alpha)*list2[i])
 
     return result
+
+def _chooseListsFromPop(population, getAttrFn):
+    list1 = list()
+    list2 = list()
+
+    for i in range(0, st.dims):
+        # Select individuals
+        rand1 = randint(0, len(population)-1)
+        rand2 = randint(0, len(population)-1)
+        # Guarantee of different individuals
+        while rand2 == rand1:
+            rand2 = randint(0, len(population)-1)
+
+        # Add attributes
+        attr1 = getAttrFn(population[rand1])
+        attr2 = getAttrFn(population[rand2])
+        list1.append(attr1)
+        list2.append(attr2)
+
+    return list1, list2
+
 
 def localDiscRecomb(p1, p2):
     childObjvar = _discListRecomb(p1.objvar, p2.objvar)
@@ -26,22 +49,24 @@ def localDiscRecomb(p1, p2):
 
     return Solution(objvar=childObjvar, mutpace=childMutpace, rot=childRot)
 
+
+
+def globalInterpRecomb(population, childAverage=False):
+    objvarLists = _chooseListsFromPop(population, (lambda x: x.objvar))
+    mutpaceLists = _chooseListsFromPop(population, (lambda x: x.mutpace))
+    rotLists = _chooseListsFromPop(population, (lambda x: x.rot))
+
+    alpha = 0.5 if childAsAverage else random()
+    
+    childObjvar = _interpListRecomb(objvarLists[0], objvarLists[1], alpha)
+    childMutpace = _interpListRecomb(mutpaceLists[0], mutpaceLists[1], alpha)
+    childRot = _interpListRecomb(rotLists[0], rotLists[1], alpha)
+
+    return Solution(objvar=childObjvar, mutpace=childMutpace, rot=childRot)
+        
 #def globalDiscRecomb(population):
-#    list1 = list()
-#    list2 = list()
-
-#    for i in range(0, st.dims):
-#        rand1 = randint(0, len(population)-1)
-#        rand2 = randint(0, len(population)-1)
-        # Guarantee of different individuals
-#        while rand2 == rand1:
-#            rand2 = randint(0, len(population)-1)
-
-
 
 #def localInterpRecomb(p1, p2, childAvegare=False):
-
-#def globalInterpRecomb(population, childAverage=False):
 
 #localHibridRecomb(p1, p2, childAverage=False):
 
