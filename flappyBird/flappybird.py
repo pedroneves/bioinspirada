b7bea3966c28c8b129ab8207474102aff8ae145b
João Gabriel Abreu <jgsma@cin.ufpe.br>
2016-06-24 20:40:25 -0300

    It's alive! It's aliiiiiiive!

--- flappyBird/flappybird.py
+++ flappyBird/flappybird.py
@@ -4,10 +4,16 @@ import setup as sp
 import utils
 from bird import Bird
 from collections import deque
-from neural_network import Neural_Network
+from neural_network import Neural_Network, NUM_WEIGHTS
 from pipe import Pipe
 from pygame.locals import *
 
+
+def testfn():
+    from random import uniform
+    weights = [uniform(-1, 1) for i in range(NUM_WEIGHTS)]
+    net = Neural_Network(weights)
+    game_loop(net)
 
 def game_loop(neural_network=None):
     pg.init()
@@ -45,13 +51,21 @@ def game_loop(neural_network=None):
             if e.type == QUIT or (e.type == KEYUP and e.key == K_ESCAPE):
                 done = True
                 break
-            elif e.type == MOUSEBUTTONUP or (e.type == KEYUP and
-                e.key in (K_UP, K_RETURN, K_SPACE)):
-                bird.jump = sp.JUMP_TIME
             elif e.type == KEYUP and e.key == K_KP_PLUS:
                 sp.SLEEP_TIME -= 5
             elif e.type == KEYUP and e.key == K_KP_MINUS:
                 sp.SLEEP_TIME += 5
+
+        to_jump = False
+        if len(pipes) == 0:
+            raise Exception("No pipes")
+        if len(pipes) == 1:
+            to_jump = evaluate(neural_network, bird, pipes[0], pipes[0])
+        else:
+            to_jump = evaluate(neural_network, bird, pipes[0], pipes[1])
+        
+        if to_jump:
+            bird.jump = sp.JUMP_TIME
 
         if bird.colides():
             done = True
@@ -109,7 +123,7 @@ def game_loop(neural_network=None):
     pg.quit()
 
 # Return true if the decision is to jump
-def evaluate(nn, pipe1, pipe2, bird):
+def evaluate(nn, bird, pipe1, pipe2):
     bird_y = bird.y
     bird_acc = bird.jump
     pipe1_x = pipe1.posx
@@ -124,4 +138,5 @@ def evaluate(nn, pipe1, pipe2, bird):
     jump = np.sign(nn_output) > 0
     return jump
 
-game_loop()
+
+#game_loop()
